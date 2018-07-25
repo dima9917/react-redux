9e7717b993241d33e7e0cc80ea06e1cf9cfd007e
dima9917 <dima9917@gmail.com>
2018-07-25 09:28:56 -0700

    initial commit

---
+++ ReduxSimpleStarter-master/src/index.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = function() {
+    return <div>Hi!</div>;
+}
+
+
+
+ReactDom.render(<App />, document.querySelector('.container'));
