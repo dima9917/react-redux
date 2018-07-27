01682a69066e749a74716430a0034ce31e97964e
dima9917 <dima9917@gmail.com>
2018-07-26 22:09:57 -0700

    S3L21

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,11 +1,18 @@
 
 import React from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyARqs1dY6a9Apba31WBySC677d3nozeBm0';
 
+YTSearch({
+    key: API_KEY,
+    term: 'surfboards'
+}, function (data) {
+    console.log(data)
+});
 
 const App = () => {
     return (
