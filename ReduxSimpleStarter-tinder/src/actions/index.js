a3283c3f9d0ab0a5e4c625c5ee1b55d05d1d859f
dima9917 <dima9917@gmail.com>
2018-08-05 15:33:06 -0700

    comment

--- ReduxSimpleStarter-tinder/src/actions/index.js
+++ ReduxSimpleStarter-tinder/src/actions/index.js
@@ -3,7 +3,7 @@ export function selectBook(book) {
     console.log('A book has been selected:', book.title)
 
     return{
-        type: 'BOOK_SELECTED',
-        payload: book
+        type: 'BOOK_SELECTED', //required 
+        payload: book           //optional
     }
 }
