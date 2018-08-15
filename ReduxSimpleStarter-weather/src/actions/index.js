0909fc7c760bbfb690a38f29f431dcbeed4b65c8
dima9917 <dima9917@gmail.com>
2018-08-15 11:16:45 -0700

    S5L57

---
+++ ReduxSimpleStarter-weather/src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+const API_KEY = "e5e357c12c11eadd402b149d73aa9e84";
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city}`;
+    const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
