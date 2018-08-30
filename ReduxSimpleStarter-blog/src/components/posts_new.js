13b5c9e6396fe061c6cc26a38a7e614189ded810
dima9917 <dima9917@gmail.com>
2018-08-30 16:59:39 -0700

    s9L133: ReduxForm

--- ReduxSimpleStarter-blog/src/components/posts_new.js
+++ ReduxSimpleStarter-blog/src/components/posts_new.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
-    render() {
-      return (
-        <div>
-          PostsNew!
-        </div>
-      );
-    }
+  renderTitleField(field) {
+    return (
+      <div>
+        <input type="text" {...field.input} />
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderTitleField} />
+      </form>
+    );
+  }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: "PostsNewForm"
+})(PostsNew);
