79cbd1108db3c82ad3ef799c3c76ac8e1586f37f
dima9917 <dima9917@gmail.com>
2018-09-06 09:52:38 -0700

    S6L89: Validation

--- ReduxSimpleStarter-blog/src/components/posts_new.js
+++ ReduxSimpleStarter-blog/src/components/posts_new.js
@@ -15,7 +15,7 @@ class PostsNew extends Component {
     return (
       <form>
         <Field label="Title" name="title" component={this.renderField} />
-        <Field label="Tags" name="tags" component={this.renderField} />
+        <Field label="Categories" name="categories" component={this.renderField} />
         <Field label="Post Content" name="content" component={this.renderField} />
 
       </form>
@@ -23,6 +23,25 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if(!values.title){
+    errors.title = "Enter a title!";
+  }
+
+  if(!values.categories){
+    errors.title = "Enter some categories!";
+  }
+
+  if(!values.content){
+    errors.title = "Enter some content please";
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: "PostsNewForm"
 })(PostsNew);
