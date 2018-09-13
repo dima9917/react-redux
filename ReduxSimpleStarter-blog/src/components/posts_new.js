6f79da1d5eefb62165d48a782c6510dc704762e9
dima9917 <dima9917@gmail.com>
2018-09-13 14:44:43 -0700

    S6L96: Navigation thru callbacks

--- ReduxSimpleStarter-blog/src/components/posts_new.js
+++ ReduxSimpleStarter-blog/src/components/posts_new.js
@@ -21,7 +21,10 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    this.props.createPost(values);
+    
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
