8b220db99354d3ba3f60f14ba2bdf749f3ededb2
dima9917 <dima9917@gmail.com>
2018-09-25 12:42:57 -0700

    S6L99

--- ReduxSimpleStarter-blog/src/components/post_show.js
+++ ReduxSimpleStarter-blog/src/components/post_show.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {fetchPost} from '../actions';
+
 class PostShow extends Component {
-    render() {
-      return (
-        <div>
-          Posts Show!
-        </div>
-      )
-    }
+
+  componentDidMount() {
+    const {id} = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
+  render() {
+    return (
+      <div>
+        Posts Show!
+      </div>
+    );
+  }
 }
 
-export default PostShow;
+function mapStateToProps({posts}, ownProps) {
+  return {post: posts[ownProps.match.params.id]};
+}
+
+export default connect(mapStateToProps, { fetchPost}) (PostShow);
